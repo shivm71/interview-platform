3e48a4b5719e10a753f41909e33f401b910454d0
SHIVAM KUMAR SHAH <shivam.shah@paysafe.com>
2022-07-03 22:56:35 +0530

    heroku 2

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+app.use(express.static('./dist/app'));
+    app.get('/*', function(req, res) {
+        res.sendFile('index.html', {root: 'dist/app/'}
+        );
+    });
 app.listen(process.env.PORT || 8080);
+
